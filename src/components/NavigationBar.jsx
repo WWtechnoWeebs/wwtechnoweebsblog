97219e92f158222ba0ca29664ac4c4d2f06fb79f
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2023-02-20 12:57:40 +0200

    Added animation

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,24 +1,35 @@
-import React from "react";
 import '../styleFolder/Navigation.css'
 
 function NavigationBar(){
     return(
         <div className="NavBar">
-            <div>
-                <button id="mainBut">WWtechnoWeebs</button>
+            <div className="mainBut">
+                <span>W</span>
+                <span>W</span>
+                <span id='techno'>t</span>
+                <span id='techno'>e</span>
+                <span id='techno'>c</span>
+                <span id='techno'>h</span>
+                <span id='techno'>n</span>
+                <span id='techno'>o</span>
+                <span id='Weebs'>W</span>
+                <span id='Weebs'>e</span>
+                <span id='Weebs'>e</span>
+                <span id='Weebs'>b</span>
+                <span id='Weebs'>s</span>
             </div>
             <div className="navigationButs" style={{display:'flex',flexDirection:'row'}}>
                 <div id="dropdown-affiliates">
-                    <button class="dropbtn">Affiliates</button>
-                    <div class="dropdown-content-affiliates">
+                    <button className="dropbtn">Affiliates</button>
+                    <div className="dropdown-content-affiliates">
                         <a href="/">Link 1</a>
                         <a href="/">Link 2</a>
                         <a href="/">Link 3</a>
                     </div>
                 </div>
                 <div id="dropdown-CVs">
-                    <button class="dropbtn">CVs</button>
-                    <div class="dropdown-content-CV">
+                    <button className="dropbtn">CVs</button>
+                    <div className="dropdown-content-CV">
                         <a href="/">Νίκου Θωμάς</a>
                         <a href="/">Πατσούρας Χρήστος</a>
                     </div>
