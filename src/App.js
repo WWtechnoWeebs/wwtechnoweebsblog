3491023aae9dcd1e22dbdddb55f429f9d3366f37
Christos Fanourios Patsouras <donchokomaster69@gmail.com>
2023-02-25 12:41:34 +0200

    Added router and Hardware

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import NavigationBar from './components/NavigationBar';
 import HomePage from './pages/HomePage';
+import {Route, Routes} from 'react-router-dom';
+import Hardware from './pages/Hardware';
 
 function App() {
   return (
     <div>
       <NavigationBar />
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/hardware' element={<Hardware/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Hardware.jsx
@@ -0,0 +1,13 @@
+import '../styleFolder/Hardware.css'
+
+function Hardware() {
+    return ( 
+        <div className='hardware'>
+            <div className='hardwarecontent'>
+                <textarea className='search'>Search</textarea>
+            </div>
+        </div>
+     );
+}
+
+export default Hardware;
