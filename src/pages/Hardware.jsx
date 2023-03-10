b2d7841c097a037260198566e419602e2fed4b14
christos fanourios patsouras <xristos.patsoyras@gmail.com>
2023-03-10 02:13:23 +0200

    Added hooks

--- src/pages/Hardware.jsx
+++ src/pages/Hardware.jsx
@@ -1,11 +1,58 @@
 import '../styleFolder/Hardware.css'
+import useFetch from '../hooks/useFetch';
 
 function Hardware() {
+    const {loading, error, data} = useFetch('http://localhost:1337/api/hard-wares') //Here goes the server link
+
+    //toLocaleDateString you get the format of date you want ex en-GB
+    function getMyDate(value, locale = 'en-GB') {
+        return new Date(value).toLocaleDateString(locale, { day:'2-digit'});
+    }
+
+    function getMyYear(value, locale = 'en-GB') {
+        return new Date(value).toLocaleDateString(locale, { year:'numeric'});
+    }
+
+    function getMyMonth(value, locale= 'en-DB') {
+        return new Date(value).toLocaleDateString(locale,{ month:'short' });
+    }
+
+    if(loading){
+        return (
+            <div className='hardware'>
+                <h1>LOADING...</h1>
+            </div>
+        );
+    }
+
+    if(error){
+        return (
+            <div className='hardware'>
+                <h1>Something went wrong...</h1>
+            </div>
+        );
+    }
+
     return ( 
         <div className='hardware'>
             <div className='hardwarecontent'>
                 <textarea className='search'>Search</textarea>
             </div>
+            
+            <div className='filters'>
+                   
+            </div>
+            <div className='articles'>
+                {data.map(frontData => (
+                    <div key={frontData.id} className="panel">
+                        <div className='date'>
+                            <h1>{getMyDate(frontData.attributes.Date)}</h1>
+                            <p>{getMyMonth(frontData.attributes.Date)}{getMyYear(frontData.attributes.Date)}</p>
+                        </div>
+                        <p className='description'>{frontData.attributes.Description}</p>
+                    </div>
+                ))}
+            </div>            
         </div>
      );
 }
